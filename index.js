6fde9e6e18b5a4e21006ade4cd242adaf381f295
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-10-05 17:27:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const IN_PROD = process.env.NODE_ENV === "production";
+
+// MIDDLEWARES
+app.use(cors())
+
+app.use(express.json({ limit: "10mb" }));
+
+app.get("/", (req, res) => {
+	console.log(req.ip);
+	res.send({ data: "Welcome" });
+});
+
+// INITIALIZE EXPRESS PORT AND LISTEN
+const port = process.env.PORT || 5000;
+app.listen(port, (err) => {
+	if (err) console.log("Error in running express : " + err);
+	else console.log("Server running on port : " + port);
+});
